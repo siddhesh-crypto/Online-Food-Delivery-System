0384647baab524a2937ce3735160a000b8236b55
siddhesh-crypto <67197685+siddhesh-crypto@users.noreply.github.com>
2024-12-24 20:21:27 +0530

    Configured spring security

---
+++ api/src/main/java/com/project/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.security.Security;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+//        http.sessionManagement(management -> management.sessionCreationPolicy((SessionCreationPolicy.STATELESS))).authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/admin/**").hasAnyRole("RESTAURANT_OWNER", "ADMIN").requestMatchers("/api/**").authenticated().anyRequest().permitAll()).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class).csrf(csrf->csrf.disable()).cors(cors -> cors.configurationSource(corsConfigurationSource));
+
+        return null;
+    }
+}
