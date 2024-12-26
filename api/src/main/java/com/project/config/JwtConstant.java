92c408f1b2b2ed89519dab806c2b0981aa6ab9fc
siddhesh-crypto <67197685+siddhesh-crypto@users.noreply.github.com>
2024-12-26 17:19:39 +0530

    configured jwt

---
+++ api/src/main/java/com/project/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.project.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "sjdvisdvsdvboibsdbkvbdsbkbvkjbkbsvk";
+    public static final String JWT_HEADER ="Authorization";
+}
